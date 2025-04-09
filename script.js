e34c56dab5564a304ce7f39123a4bf3318a63182
davideaded <david.castrodc033@gmail.com>
2025-04-09 16:57:51 -0300

    fix'd spawning position. colision with borders now is defeat. food is no longer displayed after eat it

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const SNAKE_COLOR = '#E53935';
 const FOOD_COLOR = '#456596';
 
 const canvas = document.getElementById("canvas");
-canvas.width = canvas.height = 400;
+canvas.width = canvas.height = 600;
 const ct = canvas.getContext("2d");
 
 // SCREEN
@@ -17,13 +17,13 @@ const tile = canvas.width / screenSize;
 let pos, velocity, food, snake;
 
 function init() {
-  pos = { x: 0, y: 0 };
+  pos = { x: 8, y: 10 };
   velocity = { x: 0, y: 0 };
 
   snake = [
-    { x: 8, y: 10},
-    { x: 7, y: 10},
     { x: 6, y: 10},
+    { x: 7, y: 10},
+    { x: 8, y: 10},
   ];
 
   randomFood();
@@ -68,29 +68,38 @@ function gameLoop() {
   ct.clearRect(0, 0, canvas.width, canvas.height);
   ct.fillRect(0, 0, canvas.width, canvas.height);
 
+
+  ct.fillStyle = FOOD_COLOR;
+  ct.fillRect(food.x * screenSize, food.y * screenSize, screenSize, screenSize);
+
   ct.fillStyle = SNAKE_COLOR;
   for (let cell of snake) {
     ct.fillRect(cell.x * screenSize, cell.y * screenSize, screenSize, screenSize);
   }
-
-  ct.fillStyle = FOOD_COLOR;
-  ct.fillRect(food.x * screenSize, food.y * screenSize, screenSize, screenSize);
 
   pos.x += velocity.x;
   pos.y += velocity.y;
 
   if (snake[0].x === food.x && snake[0].y === food.y) {
     snake.push({...pos});
+    pos.x += velocity.x;
+    pos.y += velocity.y;
     randomFood();
   }
   
   if (velocity.x || velocity.y) {
+    for (let cell of snake) {
+      let outboundery = cell.x >= canvas.width / screenSize || cell.x < 0 || cell.y < 0 || cell.y >= canvas.height / screenSize;
+      if (cell.x === pos.x && cell.y === pos.y || outboundery) {
+        return init();
+      }
+    }
     snake.push({...pos});
     snake.shift();
   }
-  
 }
 
 setInterval(() => {
-  // requestAnimationFrame(gameLoop);
+  requestAnimationFrame(gameLoop);
+  console.log("X: ", snake[0].x, "Y: ", snake[0].y)
 }, 1000 / frameRate)
